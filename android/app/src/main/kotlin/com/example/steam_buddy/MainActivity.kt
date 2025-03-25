d2d41116194b1d7cf2a2ac3485f83ceb33893f14
reihansyahfitra <reihansyahw@gmail.com>
2025-03-25 14:57:32 +0700

    Add HTTP dependency and implement API key retrieval in MainActivity

--- android/app/src/main/kotlin/com/example/steam_buddy/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/steam_buddy/MainActivity.kt
@@ -1,5 +1,20 @@
 package com.example.steam_buddy
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.steam_buddy/api_keys"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getSteamApiKey") {
+                result.success(BuildConfig.STEAM_API_KEY)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
